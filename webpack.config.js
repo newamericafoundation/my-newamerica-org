2cfa58482d678ab27428ae908faac718e82330e6
Peter Szerzo <peterszerzo@Peters-MacBook-Pro-3.local>
2015-11-16 14:35:56 -0500

    rebuild

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+
+	entry: './app/assets/scripts/bundle.js',
+
+	output: {
+		path: path.resolve('./public/assets/scripts'),
+		publicPath: 'http://localhost:3000/',
+		filename: 'bundle.js'
+	},
+
+	module: {
+		loaders: [
+		
+			{
+				test: /(\.js)|(\.jsx)$/,
+				loader: 'babel-loader',
+				query: {
+					presets: [ 'es2015', 'react' ]
+				}
+			},
+
+			{
+				test: /\.scss$/,
+				loaders: [ 'style', 'css', 'sass' ]
+			}
+
+		]
+	}
+
+}
